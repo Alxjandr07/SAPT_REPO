1a9155e21e45a55eac5608a66462f3ee623758d0
Moisés Antonio Panamá Murillo <moisesantoniopanamamurillo2@gmail.com>
2025-09-23 22:28:53 -0500

    cambiospanama

--- SistemAutomProcesoTitulacion/frmMenuCoordinador.Designer.cs
+++ SistemAutomProcesoTitulacion/frmMenuCoordinador.Designer.cs
@@ -80,7 +80,7 @@
             this.panelCabecera.Controls.Add(this.btnMaximizar);
             this.panelCabecera.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelCabecera.Location = new System.Drawing.Point(0, 0);
-            this.panelCabecera.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelCabecera.Margin = new System.Windows.Forms.Padding(2);
             this.panelCabecera.Name = "panelCabecera";
             this.panelCabecera.Size = new System.Drawing.Size(959, 33);
             this.panelCabecera.TabIndex = 12;
@@ -185,6 +185,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Gestion de Autoridades";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pnlAutoridades
             // 
@@ -213,6 +214,7 @@
             this.btnAutoridades.TabIndex = 1;
             this.btnAutoridades.Text = "Comunicados";
             this.btnAutoridades.UseVisualStyleBackColor = false;
+            this.btnAutoridades.Click += new System.EventHandler(this.btnAutoridades_Click);
             // 
             // pnlEstadoProceso
             // 
@@ -251,7 +253,7 @@
             this.panelInformacion.Controls.Add(this.pictureBox2);
             this.panelInformacion.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelInformacion.Location = new System.Drawing.Point(245, 33);
-            this.panelInformacion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelInformacion.Margin = new System.Windows.Forms.Padding(2);
             this.panelInformacion.Name = "panelInformacion";
             this.panelInformacion.Size = new System.Drawing.Size(714, 38);
             this.panelInformacion.TabIndex = 20;
@@ -294,7 +296,7 @@
             // 
             this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContenedor.Location = new System.Drawing.Point(245, 71);
-            this.panelContenedor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(2);
             this.panelContenedor.Name = "panelContenedor";
             this.panelContenedor.Size = new System.Drawing.Size(714, 418);
             this.panelContenedor.TabIndex = 21;
